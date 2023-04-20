34793f4321d6660cefa97a3016ece802eaee0394
miopanda <114249779+miopanda@users.noreply.github.com>
2023-04-20 16:00:28 +0900

    prototypeテーブルの再作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_20_062038) do
+ActiveRecord::Schema.define(version: 2023_04_20_064002) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -37,8 +37,10 @@ ActiveRecord::Schema.define(version: 2023_04_20_062038) do
     t.string "title", null: false
     t.text "catch_copy", null: false
     t.text "concept", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_prototypes_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -58,4 +60,5 @@ ActiveRecord::Schema.define(version: 2023_04_20_062038) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "prototypes", "users"
 end
