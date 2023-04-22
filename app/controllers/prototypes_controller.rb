9cf2e40a611752e62fd8c625db00b279fac3df44
yuna1120 <kumakumabear1120@icloud.com>
2023-04-22 16:01:39 +0900

    詳細機能実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -17,6 +17,10 @@ class PrototypesController < ApplicationController
     end
   end
 
+  def show
+    @prototype = Prototype.find(params[:id])
+  end
+
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
