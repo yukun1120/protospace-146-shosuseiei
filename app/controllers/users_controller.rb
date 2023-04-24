5f9290108f05d3ed51ba9c5f063a907d265b25e4
masakazu-fujimoto <tengbenjianghe@gmail.com>
2023-04-24 18:46:00 +0900

    ユーザー詳細ページ機能、修正１

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
-    # binding.pry
     @name = user.name
     @profile = user.profile
     @occupation = user.occupation
