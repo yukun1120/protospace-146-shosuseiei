83466aa93e6c6293560a57c628bd95ed83caaffa
masakazu-fujimoto <tengbenjianghe@gmail.com>
2023-04-24 18:15:39 +0900

    ユーザー詳細ページ機能

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @prototypes = Prototype.includes(:user)
@@ -50,6 +50,12 @@ class PrototypesController < ApplicationController
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
+    # binding.pry
     @name = user.name
     @profile = user.profile
     @occupation = user.occupation
