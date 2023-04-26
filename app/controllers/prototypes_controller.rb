44276946f976b34942462a30da93ffbbedb4158f
miopanda <114249779+miopanda@users.noreply.github.com>
2023-04-26 15:20:29 +0900

    編集機能の制限

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -56,7 +56,8 @@ class PrototypesController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in?
+    @prototype = Prototype.find(params[:id])
+    unless user_signed_in? && current_user.id == @prototype.user_id
       redirect_to action: :index
     end
   end
