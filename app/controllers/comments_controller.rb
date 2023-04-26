b5a84aafa685c7ee68337236a3e1adba33d54c2d
miopanda <114249779+miopanda@users.noreply.github.com>
2023-04-26 14:50:20 +0900

    commentsコントローラーの修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,12 @@
 class CommentsController < ApplicationController
   def create
-    comment = Comment.new(comment_params)
-    if comment.save
+    @comment = Comment.new(comment_params)
+    if @comment.save
       redirect_to prototype_path(params[:prototype_id])
+    else
+      @prototype = @comment.prototype
+      @comments = @prototype.comments.includes(:user)
+      render "prototypes/show"
     end
   end
 
